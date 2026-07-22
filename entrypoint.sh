a916779cedb60da4fbce6cbe2ee9bc3c3e8c99ba
LMS5413 <lms5413@outlook.com>
2026-07-22 12:36:54 -0300

    fix email and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 set -eu
 
-export HOST="${SERVER_IP:-0.0.0.0}"
+export HOST="0.0.0.0"
 export PORT="${SERVER_PORT:-3100}"
-export OPENCODE_ALLOW_ALL_MODELS=$([[ "$OPENCODE_ALLOW_ALL_MODELS" == "1" ]] && echo "true" || echo "false")
+
+case "${OPENCODE_ALLOW_ALL_MODELS:-true}" in
+    1|true|TRUE|yes|YES|on|ON)
+        export OPENCODE_ALLOW_ALL_MODELS=true
+        ;;
+    *)
+        export OPENCODE_ALLOW_ALL_MODELS=false
+        ;;
+esac
 
 cd /home/container
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-${MODIFIED_STARTUP}
+if [ -z "${STARTUP:-}" ]; then
+    echo "Error: the STARTUP environment variable is not set." >&2
+    exit 1
+fi
+
+MODIFIED_STARTUP=$(printf '%s' "$STARTUP" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+
+echo ":/home/container$ $MODIFIED_STARTUP"
+exec /bin/bash -c "$MODIFIED_STARTUP"
